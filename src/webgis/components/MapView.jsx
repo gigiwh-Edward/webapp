e81cf36f0d97706f86e4d4841cc49bf65466459e
531256646 <531256646@qq.com>
2018-05-23 11:15:01 +0800

    topo

--- src/webgis/components/MapView.jsx
+++ src/webgis/components/MapView.jsx
@@ -9,7 +9,7 @@ export default class MapView extends Component {
         await import('script-loader!@/maplib/init');
         const [MapView, Map] = await esriLoader.loadModules(['mapgis/views/MapView', 'mapgis/Map']);
         let map = new Map({
-            basemap: "streets"
+            basemap: "topo"
         });
         let view = new MapView({
             map: map,
